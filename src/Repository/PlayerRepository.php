da391b641958ffae24828690f2e66e2f66c54334
Freddy <freddy@adrware.mg>
2024-04-09 15:48:43 +0300

    filtre & amelioration interface

--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -21,28 +21,29 @@ class PlayerRepository extends ServiceEntityRepository
         parent::__construct($registry, Player::class);
     }
 
-//    /**
-//     * @return Player[] Returns an array of Player objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findDistinctsClub(): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->innerJoin('p.clubTeam', 'c')
+            ->select('DISTINCT c.name')
+            ->getQuery()
+            ->getArrayResult();
 
-//    public function findOneBySomeField($value): ?Player
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        $clubNames = array_column($qb, 'name');
+
+        return $clubNames;
+    }
+
+    public function findDistinctsNationalTeam(): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->innerJoin('p.nationalTeam', 'n')
+            ->select('DISTINCT n.name')
+            ->getQuery()
+            ->getArrayResult();
+
+        $nationaleTeamNames = array_column($qb, 'name');
+
+        return $nationaleTeamNames;
+    }
 }
